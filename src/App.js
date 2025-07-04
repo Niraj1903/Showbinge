09e143f2887f7d84cd14475d55fa3c0377d24645
Niraj1903 <niraj.singh1903@outlook.com>
2025-07-05 01:18:42 +0530

    components created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
   return (
     <>
-      <h3 className="text-3xl font-bold text-green-800">Hello ShowBinge</h3>
+      <Body />
     </>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <div className="text-green-800">Login</div>
+    </>
+  );
+};
+
+export default Login;
