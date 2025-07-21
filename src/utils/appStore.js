109d16384e36be732dc824b219799cda0a8345c2
Niraj1903 <niraj.singh1903@outlook.com>
2025-07-21 23:05:22 +0530

    Redux store created with userSlice

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "../utils/userSlice";
 
-const appStore = configureStore();
+const appStore = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
 
 export default appStore;
