bb16f1b37b645bf0509fdd1ea497d4f70dd7ae28
Niraj1903 <niraj.singh1903@outlook.com>
2025-08-03 22:51:36 +0530

    Image issue fix

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -6,9 +6,9 @@ import { BODY_BG_URL } from "../utils/constants";
 const GptSearch = () => {
   return (
     <>
-      <div className="-z-10 fixed">
+      {/* <div className="-z-10 fixed">
         <img className="object-cover" src={BODY_BG_URL} alt="BG-Image" />
-      </div>
+      </div> */}
       <GptSearchBar />
       <GptMovieSuggestion />
     </>
